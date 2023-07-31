824f2ee6de07589cd6ef7e496011ee547adcac41
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-07-31 08:59:46 +0530

    about1

--- src/Components/About/About1.js
+++ src/Components/About/About1.js
@@ -4,12 +4,23 @@ import '../../Css/About1.css';
 const About1 = () => {
   return (
    <main>
-      <div class="px-4 py-5 my-5 text-center" id="paddingseeting">
-          {/* <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> */}
-          <h1 class="display-5 fw-bold text-body-emphasis">Our History</h1>
-          <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <div className="container">
+        <div className="row align-items-center">
+          {/* Left Side Content */}
+          <div className="col-lg-6">
+            <div className="px-4 py-5 text-center" id="paddingseeting">
+              <h1 className="display-5 fw-bold text-body-emphasis">Our History</h1>
+              <div className="col-lg-10 mx-auto">
+                <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world's most popular front-end open-source toolkit, featuring Sass variables and mixins, a responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+              </div>
+            </div>
           </div>
+
+          {/* Right Side Image */}
+          <div className="col-lg-6">
+            <img className="d-block mx-auto mb-4" src="../img/img-benefits.webp" alt="Your Image" width="400" height="300" />
+          </div>
+        </div>
       </div>
    </main>
   );
