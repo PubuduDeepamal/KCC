060456c3b017ec87070003f8a9ff90fdee533479
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-07-31 19:07:11 +0530

    comtact

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks</span>
+          <span className='text-reset'>Get connected with us on social networks</span>
         </div>
 
         <div>
