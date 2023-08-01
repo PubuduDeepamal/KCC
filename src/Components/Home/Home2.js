7026f1f0c773d8bf0f9eca368765b6925a94aa49
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 09:55:17 +0530

    video

--- src/Components/Home/Home2.js
+++ src/Components/Home/Home2.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import '../../Css/Home2.css'
+import '../../Css/Home2.css';
 
 const Home2 = () => {
-
   return (
     <div id='container'>
-    <div className="container py-3">
+      <div className="container py-3">
+        <header>
+          <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 fw-normal text-body-emphasis" id="ParkingRatestitle">
+              <b>Why Choose KCC Car Park?</b>
+            </h1>
+          </div>
+        </header>
 
-      <header>
-        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-          <h1 class="display-4 fw-normal text-body-emphasis" id="ParkingRatestitle"><b>Why Choose KCC Car Park?</b></h1>
-        </div>
-      </header>
-
-      <main>
-       
-      </main>
-    </div>
+        <main>
+          <video controls width="100%" height="auto">
+            <source src="https://player.vimeo.com/video/57671754?h=bb983fcaec" type="video/mp4" />
+          </video>
+        </main>
+      </div>
     </div>
   );
 };
