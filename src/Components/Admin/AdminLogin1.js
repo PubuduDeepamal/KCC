0ede44993538fcf8ef54ee6ebe018de0940c5d0c
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 12:18:06 +0530

    login

--- src/Components/Admin/AdminLogin1.js
+++ src/Components/Admin/AdminLogin1.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../Css/AdminLogin.css';
+import Swal from 'sweetalert';
 
 const AdminLogin1 = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Define 3 sets of valid usernames and passwords
+    const validUsers = [
+      { username: 'user', password: '1234' },
+      { username: 'user2@example.com', password: 'password2' },
+      { username: 'user3@example.com', password: 'password3' },
+    ];
+
+    // Check if the entered username and password match any of the valid sets
+    const isValidUser = validUsers.some(
+      (user) => user.username === username && user.password === password
+    );
+
+    if (isValidUser) {
+      // Perform the login action here, e.g., redirect to a dashboard page
+      console.log('Login successful!');
+      // Replace the console.log with the action you want to take upon successful login.
+    } else {
+      // Display SweetAlert error message
+      Swal({
+        icon: 'error',
+        title: 'Invalid username or password',
+        text: 'Please check your username and password and try again.',
+      });
+    }
+  };
+
   return (
     <div id='container1'>
       <div className="container py-3">
@@ -12,14 +45,28 @@ const AdminLogin1 = () => {
         </header>
 
         <main className="form-signin w-100 m-auto">
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <div className="form-floating">
-              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
               <label htmlFor="floatingInput">Email address</label>
             </div>
-            <br></br>
+            <br />
             <div className="form-floating">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <label htmlFor="floatingPassword">Password</label>
             </div>
 
