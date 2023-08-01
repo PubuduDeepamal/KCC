d8bffc42d88566ef9c12690cb6d724f3ac84edea
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 17:04:35 +0530

    Update LocationMap.js

--- src/Components/Locatin/LocationMap.js
+++ src/Components/Locatin/LocationMap.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import '../../Css/LocationMap.css';
 
@@ -6,10 +5,19 @@ const GoogleMapComponent = () => {
   return (
     <main id="mapeliment">
         <h1 id="parkingtitle"><b>Parking Location</b></h1>
-        <iframe width="100%" height="391" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas"  className="map" src="https://maps.google.com/maps?width=1920&amp;height=391&amp;hl=en&amp;q=Sri%20Wickrama%20Rajasinghe%20Mawatha,%20Kandy%20kandy+(KCC%20Car%20Park)&amp;t=p&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-
+        <iframe
+          title="Parking Location Map"
+          width="100%"
+          height="391"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          id="gmap_canvas"
+          className="map"
+          src="https://maps.google.com/maps?width=1920&amp;height=391&amp;hl=en&amp;q=Sri%20Wickrama%20Rajasinghe%20Mawatha,%20Kandy%20kandy+(KCC%20Car%20Park)&amp;t=p&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
     </main>
-
   );
 };
 
