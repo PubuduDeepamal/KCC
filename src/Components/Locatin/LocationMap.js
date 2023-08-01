369a3a1292d6839c2abc1b5787d513a1bcd7c886
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 16:44:26 +0530

    Update LocationMap.js

--- src/Components/Locatin/LocationMap.js
+++ src/Components/Locatin/LocationMap.js
@@ -1,17 +1,6 @@
 
 import React from "react";
-// import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 import '../../Css/LocationMap.css';
-
-const containerStyle = {
-  width: "100%",
-  height: "400px",
-};
-
-const center = {
-  lat: 7.2918,
-  lng: 80.63696
-};
 
 const GoogleMapComponent = () => {
   return (
