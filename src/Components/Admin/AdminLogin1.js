a4161f313791841edf841a8bce7de2900e587d27
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 12:05:04 +0530

    sing in

--- src/Components/Admin/AdminLogin1.js
+++ src/Components/Admin/AdminLogin1.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import '../../Css/AdminLogin.css';
 
 const AdminLogin1 = () => {
-
-
   return (
     <div id='container1'>
-    <div className="container py-3">
+      <div className="container py-3">
+        <header>
+          <div className="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 className="display-4 fw-normal text-body-emphasis" id="ParkingRatestitle"><b>Please sign in</b></h1>
+          </div>
+        </header>
 
-      <header>
-        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-          <h1 class="display-4 fw-normal text-body-emphasis" id="ParkingRatestitle"><b>Current Details</b></h1>
-          <p class="fs-5 text-body-secondary">(All data presented in that section is live data.)</p>
-        </div>
-      </header>
+        <main className="form-signin w-100 m-auto">
+          <form>
+            <div className="form-floating">
+              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
 
-      <main>
-        
-      </main>
-    </div>
+            <div className="form-check text-start my-3">
+              <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Remember me
+              </label>
+            </div>
+            <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+          </form>
+        </main>
+      </div>
     </div>
   );
 };
