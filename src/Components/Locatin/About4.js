8d5a5a49b3329f5b6b6604b749074627ae224c83
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 05:40:36 +0530

    LOCtion

---
+++ src/Components/Locatin/About4.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import parkingData from '../../JSON/About4.json';
+import '../../Css/About4.css';
+
+const About4 = () => {
+  const dataBundles = Object.values(parkingData);
+
+  return (
+    <div id='container2'>
+     <br></br> <br></br> 
+      <h1 id="blogtitle"><b>Parking Amenities</b></h1>
+      <div className="container py-3">
+        <main>
+          <div className="row row-cols-4 mb-3 text-center">
+            {dataBundles.map((bundle, index) => (
+              <div className="col col-md-3 col-12" key={index} id="margintop">
+                <div className="card mb-4 rounded-3 shadow-sm" id='box'>
+                  {/* Display the image using the <img> tag */}
+                  <img 
+                    id='ICON'
+                    src={bundle.imageSrc} // Assuming bundle.imageSrc contains the URL of the image
+                    alt={`Bundle ${index + 1}`} // Optional: Provide an alt text for accessibility
+                    className="card-img-top" // Optional: Add a class for styling
+                  />
+                  <div className="card-body">
+                    <h1 className="card-title pricing-card-title" id="font">
+                      {bundle.price}
+                    </h1>
+                    <ul className="list-unstyled mt-3 mb-4" id="font">
+                      {bundle.description.split('\n').map((item, index) => (
+                        <li key={index}>{item}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default About4;
--- src/pages/Locatin.js
+++ src/pages/Locatin.js
@@ -1,11 +1,13 @@
 import LocationHero from '../Components/Locatin/LocationHero'
 import LocationMap from '../Components/Locatin/LocationMap'
+import About4 from '../Components/Locatin/About4'
 
 function Locatin() {
   return (
     <div>
         <LocationHero/>
         <LocationMap/>
+        <About4/>
     </div>
   
   );
