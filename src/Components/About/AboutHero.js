a30c776f03a9c9944c6e0bf426c8f03f70093351
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-01 16:40:19 +0530

    Update AboutHero.js

--- src/Components/About/AboutHero.js
+++ src/Components/About/AboutHero.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import '../../Css/Hero.css';
 import '../../Css/AboutHero.css';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 
   const AboutHero = () => {
   return (
