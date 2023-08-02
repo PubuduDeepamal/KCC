f833dbbcac80e0f08e35379ac8600071af1e73b4
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-03 04:41:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 
-import {BrowserRouter as Link, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Booking from './pages/Booking'
-import Locatin from './pages/Locatin'
-import Contacut from './pages/Contacut'
-import AdminLogin from './pages/Admin/AdminLogin'
-import Footer from './Components/Footer/Footer'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Booking from './pages/Booking';
+import Locatin from './pages/Locatin';
+import Contacut from './pages/Contacut';
+import AdminLogin from './pages/Admin/AdminLogin';
+import Footer from './Components/Footer/Footer';
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element = {<Home/>}/>
-        <Route path='/about' element = {<About/>}/>
-        <Route path='/Locatin' element = {<Locatin/>}/>
-        <Route path='/contacut' element = {<Contacut/>}/>
-        <Route path='/booking' element = {<Booking/>}/>
-        <Route path='/AdminLogin' element = {<AdminLogin/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Locatin' element={<Locatin />} />
+        <Route path='/contacut' element={<Contacut />} />
+        <Route path='/booking' element={<Booking />} />
+        <Route path='/AdminLogin' element={<AdminLogin />} />
       </Routes>
       <Footer />
     </>
