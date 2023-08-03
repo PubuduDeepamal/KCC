bd6bab842547c1d8d1d98d46ebb960e37b03a629
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-03 15:26:44 +0530

    home4

--- src/Components/Home/Home4.js
+++ src/Components/Home/Home4.js
@@ -1,22 +1,17 @@
-
 import React from 'react';
 import '../../Css/Home4.css'
 
 const Home4 = () => {
-
-
   return (
-    <div id='container1'>
-    <div className="container py-3">
-
-      <header>
-       
-      </header>
-
-      <main>
-        
-      </main>
-    </div>
+    <div id='containerhome4'>
+      <div className="container py-3">
+        <main>
+          <div className="d-flex align-items-center">
+            <h1 id="contenthOME4">Save money by pre-booking online</h1>
+            <button className="btn btn-primary ml-3" id="bookhome4">Book Now</button>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
