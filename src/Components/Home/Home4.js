7736fca66e58bfb1298b648961fcc4f91c640925
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-03 15:16:12 +0530

    home

---
+++ src/Components/Home/Home4.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import '../../Css/Home4.css'
+
+const Home4 = () => {
+
+
+  return (
+    <div id='container1'>
+    <div className="container py-3">
+
+      <header>
+       
+      </header>
+
+      <main>
+        
+      </main>
+    </div>
+    </div>
+  );
+};
+
+export default Home4;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ import Home2 from '../Components/Home/Home2'
 import Home3 from '../Components/Home/Home3'
 import ParkingRates from '../Components/Home/ParkingRates'
 import Header1 from '../Components/Heder/Header1'
+import Home4 from '../Components/Home/Home4'
 import '../Css/Footer.css';
 
 function Home() {
@@ -15,6 +16,7 @@ function Home() {
       <Home2/>
       <Home3/>
       <ParkingRates/>
+      <Home4/>
     </div>
     
   );
