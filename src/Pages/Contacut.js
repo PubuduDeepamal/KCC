4f3aa1fc8ec12e91e31cc96266ec195893c598e9
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-05 19:36:20 +0530

    Update Contacut.js

--- src/Pages/Contacut.js
+++ src/Pages/Contacut.js
@@ -3,6 +3,7 @@ import Contactus from '../Components/Contact/Contactus'
 import LocationMap from '../Components/Locatin/LocationMap'
 import Header1 from '../Components/Heder/Header1'
 import '../Css/Footer.css';
+import Footer from '../Components/Footer/Footer';
 
 function Contacut() {
   return (
@@ -11,6 +12,7 @@ function Contacut() {
         <ContactHero/>
         <Contactus/>
         <LocationMap/>
+        <Footer />
     </div>
   
   );
