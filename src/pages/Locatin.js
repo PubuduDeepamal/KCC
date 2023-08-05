5a8641947dc72574e376aa1602dded3d9fbabd6e
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-05 19:34:48 +0530

    Update Locatin.js

--- src/pages/Locatin.js
+++ src/pages/Locatin.js
@@ -2,6 +2,7 @@ import LocationHero from '../Components/Locatin/LocationHero'
 import LocationMap from '../Components/Locatin/LocationMap'
 import About4 from '../Components/Locatin/About4'
 import Header1 from '../Components/Heder/Header1'
+import Footer from '../Components/Footer/Footer';
 
 function Locatin() {
   return (
@@ -10,6 +11,7 @@ function Locatin() {
         <LocationHero/>
         <LocationMap/>
         <About4/>
+        <Footer />
     </div>
   
   );
