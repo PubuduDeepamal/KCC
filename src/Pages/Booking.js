6c172f4e5f5d28af47926e6a8b7e580d493b1562
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-05 19:36:13 +0530

    Update Booking.js

--- src/Pages/Booking.js
+++ src/Pages/Booking.js
@@ -1,11 +1,13 @@
 import BookingHero from '../Components/Booking/BookingHero'
 import Header1 from '../Components/Heder/Header1'
+import Footer from '../Components/Footer/Footer';
 
 function Booking() {
   return (
     <div>
       <Header1/>
       <BookingHero/>
+      <Footer />
     </div>
     
   );
