3de437abeb1ea92ed1f01fe223a3c4f2746bca3d
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-05 19:36:08 +0530

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,7 @@ import About1 from '../Components/About/About1'
 import About2 from '../Components/About/About2'
 import About3 from '../Components/About/About3'
 import Header1 from '../Components/Heder/Header1'
+import Footer from '../Components/Footer/Footer';
 
 function About() {
   return (
@@ -13,6 +14,7 @@ function About() {
       <About1/>
       <About2/>
       <About3/>
+      <Footer />
     </div>
     
   );
