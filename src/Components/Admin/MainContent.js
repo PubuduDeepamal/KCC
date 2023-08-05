30e02877861daa80d8d83ab69671ff1407064769
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-05 18:28:38 +0530

    Update

--- src/Components/Admin/MainContent.js
+++ src/Components/Admin/MainContent.js
@@ -4,16 +4,7 @@ import React from 'react';
 function MainContent({ selectedMenuItem }) {
   // Render different content based on the selected menu item
   const renderContent = () => {
-    switch (selectedMenuItem) {
-      case 'overview':
-        return <div>Overview Content</div>;
-      case 'analytics':
-        return <div>Analytics Content</div>;
-      case 'settings':
-        return <div>Settings Content</div>;
-      default:
-        return <div>Invalid Selection</div>;
-    }
+    
   };
 
   return <div className="main-content">{renderContent()}</div>;
