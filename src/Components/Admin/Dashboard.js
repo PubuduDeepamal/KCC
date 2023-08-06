db90b09fb55134cc9d6f98942332a0a4a45a67b9
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-06 15:10:57 +0530

    Update Dashboard.js

--- src/Components/Admin/Dashboard.js
+++ src/Components/Admin/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../Css/Sidebar.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import Costcal from '../../Components/Booking/Costcal'
 
 const Dashboard = () => {
   const handleLogout = () => {
@@ -65,31 +66,8 @@ const Dashboard = () => {
         </Col>
 
         <Col md={9} className="contact-form">
-          <div className="container px-4 py-5">
-            <Row className="row-cols-1 row-cols-lg-3">
-              <div className="feature-box p-4 border1 rounded1 mb-5" id="featured-30" >
-                <h3 className="fs-2 text-body-emphasis" id="titleicon">
-                  <b> 1160</b>
-                </h3>
-                <p id="titleicon"> Total Parking Slots</p>
-              </div>
-
-              <div className="feature-box p-4 border2 rounded1 mb-5" id="featured-31">
-                <h3 className="fs-2 text-body-emphasis" id="titleicon31">
-                  <b>60</b>
-                </h3>
-                <p id="titleicon">Number of full Parking Slots</p>
-              </div>
-
-              <div className="feature-box p-4 border3 rounded1 mb-5" id="featured-32">
-                <h3 className="fs-2 text-body-emphasis" id="titleicon31">
-                  <b>1100 </b>
-                </h3>
-                <p id="titleicon">Avalible  Parking Slots</p>
-              </div>
-
-            </Row>
-          </div>
+          <br></br><br></br>
+                <Costcal/>
         </Col>
       </Row>
     </Container>
