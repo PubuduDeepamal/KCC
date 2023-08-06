eab179f3450926a1791b8b5f45a3e5f019cb2c1b
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-06 14:56:49 +0530

    Create Admincostcal.js

---
+++ src/pages/Admin/Admincostcal.js
@@ -0,0 +1,13 @@
+import Dashboard from '../../Components/Admin/Dashboard'
+
+
+function Admincostcal() {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+    
+  );
+}
+
+export default Admincostcal;
