168910bcf5c7a9c75d95a1a3fd5001fbb793306c
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-06 14:56:57 +0530

    Update AdminDashBord.js

--- src/pages/Admin/AdminDashBord.js
+++ src/pages/Admin/AdminDashBord.js
@@ -1,14 +1,10 @@
 import Sidebar from '../../Components/Admin/Sidebar'
-import Dashboard from '../../Components/Admin/Dashboard'
-// import MainContent from '../../Components/Admin/MainContent'
 
 
 function AdminDashBord() {
   return (
     <div>
       <Sidebar/>
-      <Dashboard/>
-      {/* <MainContent/> */}
     </div>
     
   );
